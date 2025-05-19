39b4b5131290db57503dd9617ef5a565a05237b7
redzzzi <myqueen1346@naver.com>
2025-05-20 02:36:28 +0900

    👔 기존 storage 뷰와 버튼을 연결

--- app/src/main/java/com/voicescout/voicescout_android/MainActivity.kt
+++ app/src/main/java/com/voicescout/voicescout_android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.voicescout.voicescout_android
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.voicescout.voicescout_android.storage.StorageActivity
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.test_btn_1).setOnClickListener {
+        findViewById<LinearLayout>(R.id.btn_storage_view).setOnClickListener {
             val intent = Intent(this, StorageActivity::class.java)
             startActivity(intent)
         }

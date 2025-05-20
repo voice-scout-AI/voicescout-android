4e3a476414c169bc6e96a7706a2a670da05627dd
redzzzi <myqueen1346@naver.com>
2025-05-20 20:53:08 +0900

    ✨  DrawerLayout 구현 - toolbar - navigation

--- app/src/main/java/com/voicescout/voicescout_android/MainActivity.kt
+++ app/src/main/java/com/voicescout/voicescout_android/MainActivity.kt
@@ -3,20 +3,37 @@ package com.voicescout.voicescout_android
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.voicescout.voicescout_android.storage.StorageActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var menuButton: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        menuButton = findViewById(R.id.toolbar_menu)
+
         findViewById<LinearLayout>(R.id.btn_storage_view).setOnClickListener {
             val intent = Intent(this, StorageActivity::class.java)
             startActivity(intent)
+        }
+
+        menuButton.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
         }
     }
 }
